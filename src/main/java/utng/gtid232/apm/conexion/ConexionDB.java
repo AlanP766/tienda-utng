daf72111080be03c47e6a82fa1a41d63d37f451e
Alan Palomino Mares <1225100350.apm@gmail.com>
2026-08-06 22:36:47 -0600

    feat(conexion): agregar ConexionDB para MySQL utng_tienda

---
+++ src/main/java/utng/gtid232/apm/conexion/ConexionDB.java
@@ -0,0 +1,12 @@
+package utng.gtid232.apm.conexion;
+
+public class ConexionDB {
+    private static final String URL = "jdbc:mysql://localhost:3306/utng_tienda";
+    private static final String USER = "root";
+    private static final String PASSWORD = ""; 
+
+    public static Object obtenerConexion() {
+        // Estructura lista sin credenciales expuestas
+        return null;
+    }
+}
